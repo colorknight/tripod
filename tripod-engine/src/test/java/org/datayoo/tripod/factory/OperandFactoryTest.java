697b44ce69325126150d64802673219d9fbed8a3
colorknight <ttd_ttt@sina.com.cn>
2020-08-23 19:08:41 +0800

    修正包名

--- tripod-engine/src/test/java/org/datayoo/tripod/factory/OperandFactoryTest.java
+++ tripod-engine/src/test/java/org/datayoo/tripod/factory/OperandFactoryTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.datayoo.tripod.operand.factory;
+package org.datayoo.tripod.factory;
 
 import junit.framework.TestCase;
 import org.datayoo.tripod.*;
